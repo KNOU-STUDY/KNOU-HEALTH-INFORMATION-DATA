3b233ecc2600a7bed741eedea81c84bb12acb622
kukaro <justkukaro@naver.com>
2021-09-19 16:17:52 +0900

    feat:adding ch1

---
+++ ch1/math_and_physics.R
@@ -0,0 +1,9 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: justk
+# Created on: 2021-09-19
+
+math <- c(66, 64, 48, 78, 60, 90, 50, 66, 70)
+physics <- c(70, 68, 46, 84, 64, 92, 52, 68, 72)
+plot(math, physics, main='math and physics scatterplot')
+cor(math, physics)

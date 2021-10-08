34f2e981394dcd3be8a8a6b896fe73780f48bd0d
kukaro <justkukaro@naver.com>
2021-10-08 19:30:55 +0900

    feat:adding question1

--- assignment/question1/answer1.R
+++ assignment/question1/answer1.R
@@ -9,4 +9,10 @@ df <- data.frame(
     list(cancer = cancer, none_cancer = none_cancer),
     row.names = c('smoking', 'none_smoking')
 )
+'cancer_odds'
+(cancer_odds <- (df[1, 1] / sum(df[, 1])) / (df[1, 2] / sum(df[, 1])))
+'none_cancer_odds'
+(none_cancer_odds <- (df[2, 1] / sum(df[, 2])) / (df[2, 2] / sum(df[, 2])))
+'odds_rate'
+(odds_rate <- cancer_odds / none_cancer_odds)
 df

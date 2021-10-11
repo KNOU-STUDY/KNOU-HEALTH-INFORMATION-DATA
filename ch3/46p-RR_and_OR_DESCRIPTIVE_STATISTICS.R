18a857051becd1126627986f97cc4ad4c6293cd7
Coeat.k <justkukaro@naver.com>
2021-10-12 08:25:56 +0900

    feat:adding descriptive statistic

---
+++ ch3/46p-RR_and_OR_DESCRIPTIVE_STATISTICS.R
@@ -0,0 +1,18 @@
+data <- read.table('./ch3/담즙-과포화비율자료.txt', header = T, fileEncoding = 'utf-8')
+data
+attach(data)
+par(mfrow = c(2, 1))
+plot(
+  data$`과포화비율`,
+  type = 'p',
+  xlab = '자료',
+  ylab = '담즙과포화비율',
+  main = '담즙과포화비율',
+)
+plot(
+  data$`과포화비율`,
+  type = 'h',
+  xlab = '자료',
+  ylab = '담즙과포화비율',
+  main = '담즙과포화비율',
+)

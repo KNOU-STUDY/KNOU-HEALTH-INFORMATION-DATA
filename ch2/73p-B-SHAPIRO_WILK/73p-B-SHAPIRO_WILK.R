1f174c43b13921ea8f213593957ee0380c9639aa
coaet.k <justkukaro@naver.com>
2021-10-15 02:51:44 +0900

    adding: shapiro wilk

---
+++ ch2/73p-B-SHAPIRO_WILK/73p-B-SHAPIRO_WILK.R
@@ -0,0 +1,17 @@
+data <- read.table('./ch2/담즙-과포화비율자료.txt', header = T, fileEncoding = 'utf-8')
+qqnorm(data$`과포화비율`, ylab = '담즙과포화비율', main = '', col = 2)
+qqline(data$`과포화비율`, lty = 2, col = 3)
+shapiro.test(data$`과포화비율`)
+'
+	Shapiro-Wilk normality test
+
+data:  data$과포화비율
+W = 0.97392, p-value = 0.2261
+
+
+	Shapiro-Wilk normality test
+
+data:  data$과포화비율
+W = 0.97392, p-value = 0.2261
+'
+#0.05보다 크므로 기각하지못한다. 따라서 정규분포이다.
